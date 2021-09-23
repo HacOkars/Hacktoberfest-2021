fdbc76023115a3c1f8073bc056bc276ee7dfd211
Zeo-shark <sauravberaiit@gmail.com>
2021-09-24 01:13:10 +0530

    Added rituraj folder

---
+++ RituRajGupta21/hello.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class hello
+{
+    public static void main(String args[])
+    {
+        int i=2;
+        while(i+2==4)
+        {
+            System.out.println("Hello Hacktoberfest-2021");
+        }
+    }
+}
