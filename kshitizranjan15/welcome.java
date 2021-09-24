7376052b4429ec1b90d59afd4f6bda90cf979ad9
KSHITIZ RANJAN <54390873+kshitizranjan15@users.noreply.github.com>
2021-09-24 12:06:15 +0530

    Create welcome.java

---
+++ kshitizranjan15/welcome.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class welcome_iterians
+{
+    public static void main(String args[])
+    {
+        int i=2;
+        while(i+2==4)
+        {
+            System.out.println("All of you are welcome in Hacktoberfest-2021......");
+        }
+    }
+}
