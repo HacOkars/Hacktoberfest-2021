6365a8e614b1abcb613b7b7a30227a1ac5b4c0a3
architagg <architaggarwal21@gmail.com>
2021-10-01 18:07:24 +0530

    Added my CPP codes

---
+++ Architagg/helloworld.cpp
@@ -0,0 +1,15 @@
+// Simple C++ program to display "Hello World"
+
+#include<iostream>
+ 
+using namespace std;
+ 
+// main function -
+// where the execution of program begins
+int main()
+{
+    // prints hello world
+    cout<<"Hello World";
+     
+    return 0;
+}
