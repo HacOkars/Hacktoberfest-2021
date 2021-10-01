001b07cba833d39495bfe4d3b5751339cd100458
Arman-ali-khan-786 <alikhanarman786@gmail.com>
2021-10-01 10:25:38 +0530

    added BLACKCEL problem solution

---
+++ Arman-ali-khan-786/BLACKCEL.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+public class BLACKCEL
+{
+public static void main(String[] args) throws IOException
+	{
+		try{
+		    Scanner sc = new Scanner(System.in);
+		    int n = sc.nextInt();
+		    System.out.println((int)((n*n)/2));
+		}catch(Exception e){
+			return;
+		}
+	}
+}
