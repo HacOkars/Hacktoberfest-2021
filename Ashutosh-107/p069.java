8427689354dabc2a2290b90276cfd71f3b39f877
Ashutosh-107 <85955760+Ashutosh-107@users.noreply.github.com>
2021-10-01 12:55:16 +0530

    Create p069.java

---
+++ Ashutosh-107/p069.java
@@ -0,0 +1,23 @@
+public final class p069 implements EulerSolution {
+	
+	public static void main(String[] args) {
+		System.out.println(new p069().run());
+	}
+	
+	
+	private static final int LIMIT = Library.pow(10, 6);
+	
+	public String run() {
+		int maxNumer = 0;
+		int maxDenom = 1;
+		int[] totients = Library.listTotients(LIMIT);
+		for (int n = 1; n < totients.length; n++) {
+			if ((long)n * maxDenom > (long)maxNumer * totients[n]) {
+				maxNumer = n;
+				maxDenom = totients[n];
+			}
+		}
+		return Integer.toString(maxNumer);
+	}
+	
+}
