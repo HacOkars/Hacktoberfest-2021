b78b545a7620df43f5437b93f897ee9ab263cb94
nazmeevi <ildar.nazmeev@tipico.com>
2021-10-02 14:36:31 +0200

    perfect squares algo impl

--- ildar10/PerfectSquares.java
+++ ildar10/PerfectSquares.java
@@ -1,12 +1,12 @@
 public class PerfectSquares {
     public int numSquares(int n) {
-        int[] dp = new int[ n +1];
+        int[] dp = new int[n + 1];
         dp[1] = 1;
         for (int i = 2; i <= n; i++) {
-            int min = dp[i-1]+1;
-            for (int j = 1; j*j <= i; j++) {
-                if (dp[i - j*j] + 1 < min) {
-                    min = dp[i-j*j] +1;
+            int min = dp[i - 1] + 1;
+            for (int j = 1; j * j <= i; j++) {
+                if (dp[i - j * j] + 1 < min) {
+                    min = dp[i - j * j] + 1;
                 }
                 dp[i] = min;
             }
