e8b7e181d5c48062719bc595f9ab23f41c280586
nazmeevi <ildar.nazmeev@tipico.com>
2021-10-02 14:43:03 +0200

    rotate string algo

---
+++ ildar10/RotateString.java
@@ -0,0 +1,6 @@
+public class RotateString {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() !=goal.length()) return false;
+        return (s+s).contains(goal);
+    }
+}
