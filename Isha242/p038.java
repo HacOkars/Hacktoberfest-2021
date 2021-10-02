c6521bdee8b0caffaa092b3199d22cbc8ad9e313
Isha <71626410+Isha242@users.noreply.github.com>
2021-10-02 21:24:05 +0530

    Create p038.java

---
+++ Isha242/p038.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+
+public final class p038 implements EulerSolution {
+	
+	public static void main(String[] args) {
+		System.out.println(new p038().run());
+	}
+	
+	
+	public String run() {
+		int max = -1;
+		for (int n = 2; n <= 9; n++) {
+			for (int i = 1; i < Library.pow(10, 9 / n); i++) {
+				String concat = "";
+				for (int j = 1; j <= n; j++)
+					concat += i * j;
+				if (isPandigital(concat))
+					max = Math.max(Integer.parseInt(concat), max);
+			}
+		}
+		return Integer.toString(max);
+	}
+	
+	
+	private static boolean isPandigital(String s) {
+		if (s.length() != 9)
+			return false;
+		char[] temp = s.toCharArray();
+		Arrays.sort(temp);
+		return new String(temp).equals("123456789");
+	}
+	
