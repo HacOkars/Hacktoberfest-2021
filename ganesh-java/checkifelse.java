fc7329006ce8deab5d4ad02a4b9e70c59598093e
ganeshmajhi <ganeshmajhi@gmail.com>
2021-10-16 11:19:14 +0530

    added if else statement in java

---
+++ ganesh-java/checkifelse.java
@@ -0,0 +1,14 @@
+//A Java Program to demonstrate the use of if-else statement.  
+//It is a program of odd and even number.  
+public class checkifelse {  
+public static void main(String[] args) {  
+    //defining a variable  
+    int number=13;  
+    //Check if the number is divisible by 2 or not  
+    if(number%2==0){  
+        System.out.println("even number");  
+    }else{  
+        System.out.println("odd number");  
+    }  
+}  
+}  
