d249be924bf1f7b2d9a7239acdf006799a2648ab
Sejzz <91632881+Sejzz@users.noreply.github.com>
2021-10-25 11:40:53 +0530

    if_statements.java

---
+++ if_statements.java
@@ -0,0 +1,23 @@
+public class if_statements {
+    
+public static void main(String[] args) {
+           
+            int age = 19;
+            
+            if(age==75) {
+                System.out.println("Ok Boomer!");
+            }
+            else if(age>=18) {
+                System.out.println("You are an adult!");
+            }
+            else if(age>=13) {
+                System.out.println("You are a teenager!");
+            }
+            else {
+                System.out.println("You are not an adult!");
+            }
+                    
+        }
+    
+    
+}
